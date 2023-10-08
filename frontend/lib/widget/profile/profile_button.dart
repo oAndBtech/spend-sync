8145b82f3ee6f6b393a0cac63da37ff7ee288db2
Bhaksar <bhaskarmandal369@gmail.com>
2023-10-09 03:35:44 +0530

    few changes in profile button

--- frontend/lib/widget/profile/profile_button.dart
+++ frontend/lib/widget/profile/profile_button.dart
@@ -23,44 +23,55 @@ class ProfileButton extends StatelessWidget {
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
 
-    return Container(
-      height: 61,
-      width: width * 0.85,
-      decoration: BoxDecoration(
-          color: const Color(0xff222831).withOpacity(0.6),
-          borderRadius: buttonType == 1
-              ? BorderRadius.circular(36)
-              : buttonType == 2
-                  ? const BorderRadius.only(
-                      topLeft: Radius.circular(36),
-                      topRight: Radius.circular(36))
-                  : buttonType == 3
-                      ? const BorderRadius.only(
-                          bottomLeft: Radius.circular(36),
-                          bottomRight: Radius.circular(36))
-                      : BorderRadius.circular(0)),
-      child: Row(
-        children: [
-          const SizedBox(width: 24),
-          icon,
-          const SizedBox(width: 12),
-          Text(
-            buttonText,
-            style: GoogleFonts.inter(
-                color: const Color(0xffEEEEEE),
-                fontSize: 18.0,
-                letterSpacing: -0.2,
-                fontWeight: FontWeight.w400),
-          ),
-          const SizedBox(width: 144),
-          isArrow
-              ? const Icon(
-                  FontAwesomeIcons.chevronRight,
-                  color: Color(0xffEEEEEE),
-                  size: 16,
+    return InkWell(
+      onTap: onTap,
+      borderRadius: BorderRadius.circular(36),
+      child: Container(
+        height: 61,
+        width: width * 0.85,
+        decoration: BoxDecoration(
+            color: const Color(0xff222831).withOpacity(0.6),
+            borderRadius: buttonType == 1
+                ? BorderRadius.circular(36)
+                : buttonType == 2
+                    ? const BorderRadius.only(
+                        topLeft: Radius.circular(36),
+                        topRight: Radius.circular(36))
+                    : buttonType == 3
+                        ? const BorderRadius.only(
+                            bottomLeft: Radius.circular(36),
+                            bottomRight: Radius.circular(36))
+                        : BorderRadius.circular(0)),
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          children: [
+    
+            Padding(
+              padding: const EdgeInsets.only(left: 24.0),
+              child: icon,
+            ),
+            const SizedBox(width: 12),
+            Text(
+              buttonText,
+              style: GoogleFonts.inter(
+                  color: const Color(0xffEEEEEE),
+                  fontSize: 18.0,
+                  letterSpacing: -0.2,
+                  fontWeight: FontWeight.w400),
+            ),
+            const Spacer(),
+            isArrow
+                ? const Padding(
+                  padding:  EdgeInsets.only(right: 24),
+                  child:  Icon(
+                      FontAwesomeIcons.chevronRight,
+                      color: Color(0xffEEEEEE),
+                      size: 16,
+                    ),
                 )
-              : const SizedBox.shrink(),
-        ],
+                : const SizedBox.shrink(),
+          ],
+        ),
       ),
     );
   }
