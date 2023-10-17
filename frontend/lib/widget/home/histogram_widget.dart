cf710c7ce17ffc949ee69c067e7f13a5dfafc467
Bhaksar <bhaskarmandal369@gmail.com>
2023-10-18 03:55:44 +0530

    histogram done :) (may be)

---
+++ frontend/lib/widget/home/histogram_widget.dart
@@ -0,0 +1,250 @@
+import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:spend_sync/const/colors.dart';
+
+class HistogramWidget extends StatefulWidget {
+  const HistogramWidget({super.key});
+
+  final Color dark = Colors.red;
+  final Color normal = Colors.red;
+  final Color light = Colors.red;
+
+  @override
+  State<HistogramWidget> createState() => _HistogramWidgetState();
+}
+
+class _HistogramWidgetState extends State<HistogramWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+      aspectRatio: 1.66,
+      child: Padding(
+        padding: const EdgeInsets.only(top: 16),
+        child: LayoutBuilder(
+          builder: (context, constraints) {
+            final barsSpace = 4.0 * constraints.maxWidth / 200;
+            final barsWidth = 8.0 * constraints.maxWidth / 120;
+            return BarChart(
+              BarChartData(
+                alignment: BarChartAlignment.center,
+                barTouchData: BarTouchData(
+                  enabled: true,
+                ),
+                titlesData: FlTitlesData(
+                  show: true,
+                  bottomTitles: AxisTitles(
+                    sideTitles: SideTitles(
+                      showTitles: true,
+                      reservedSize: 28,
+                      getTitlesWidget: bottomTitles,
+                    ),
+                  ),
+                  leftTitles: AxisTitles(
+                    sideTitles: SideTitles(
+                      showTitles: true,
+                      reservedSize: 40,
+                      getTitlesWidget: leftTitles,
+                    ),
+                  ),
+                  topTitles: const AxisTitles(
+                    sideTitles: SideTitles(showTitles: false),
+                  ),
+                  rightTitles: const AxisTitles(
+                    sideTitles: SideTitles(showTitles: false),
+                  ),
+                ),
+                gridData: FlGridData(
+                  show: true,
+                  checkToShowHorizontalLine: (value) => value % 10 == 0,
+                  getDrawingHorizontalLine: (value) => FlLine(
+                    color: darkModeColors().secondary.withOpacity(0.4),
+                    strokeWidth: 1,
+                  ),
+                  drawVerticalLine: false,
+                ),
+                borderData: FlBorderData(
+                  show: true,
+                ),
+                // groupsSpace: barsSpace,
+                barGroups: getData(barsWidth, barsSpace),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  Widget bottomTitles(double value, TitleMeta meta) {
+    TextStyle style = GoogleFonts.inter(
+        fontSize: 15, color: darkModeColors().textColor, letterSpacing: -0.2);
+    String text;
+    switch (value.toInt()) {
+      case 0:
+        text = 'Sun';
+        break;
+      case 1:
+        text = 'Mon';
+        break;
+      case 2:
+        text = 'Tue';
+        break;
+      case 3:
+        text = 'Wed';
+        break;
+      case 4:
+        text = 'Thu';
+        break;
+      case 5:
+        text = 'Fri';
+        break;
+      case 6:
+        text = 'Sat';
+        break;
+      default:
+        text = '';
+        break;
+    }
+    return SideTitleWidget(
+      axisSide: meta.axisSide,
+      child: Text(text, style: style),
+    );
+  }
+
+  Widget leftTitles(double value, TitleMeta meta) {
+    if (value == meta.max) {
+      return Container();
+    }
+   TextStyle style = GoogleFonts.inter(
+        fontSize: 15, color: darkModeColors().textColor, letterSpacing: -0.2);
+    return SideTitleWidget(
+      axisSide: meta.axisSide,
+      child: Text(
+        meta.formattedValue,
+        style: style,
+      ),
+    );
+  }
+
+  List<BarChartGroupData> getData(double barsWidth, double barsSpace) {
+    return [
+      BarChartGroupData(
+        x: 0,
+        barsSpace: barsSpace,
+        barRods: [
+          BarChartRodData(
+            toY: 601,
+            rodStackItems: [
+              BarChartRodStackItem(0, 2000000000, widget.dark),
+              BarChartRodStackItem(2000000000, 12000000000, widget.normal),
+              BarChartRodStackItem(12000000000, 17000000000, widget.light),
+            ],
+            borderRadius: BorderRadius.zero,
+            width: barsWidth,
+          ),
+        ],
+      ),
+      BarChartGroupData(
+        x: 1,
+        barsSpace: barsSpace,
+        barRods: [
+          BarChartRodData(
+            toY: 400,
+            rodStackItems: [
+              BarChartRodStackItem(0, 2000000000, widget.dark),
+              BarChartRodStackItem(2000000000, 12000000000, widget.normal),
+              BarChartRodStackItem(12000000000, 17000000000, widget.light),
+            ],
+            borderRadius: BorderRadius.zero,
+            width: barsWidth,
+          ),
+        ],
+      ),
+      BarChartGroupData(
+        x: 2,
+        barsSpace: barsSpace,
+        barRods: [
+          BarChartRodData(
+            toY: 230,
+            rodStackItems: [
+              BarChartRodStackItem(0, 2000000000, widget.dark),
+              BarChartRodStackItem(2000000000, 12000000000, widget.normal),
+              BarChartRodStackItem(12000000000, 17000000000, widget.light),
+            ],
+            borderRadius: BorderRadius.zero,
+            width: barsWidth,
+          ),
+          
+        ],
+      ),
+      BarChartGroupData(
+        x: 3,
+        barsSpace: barsSpace,
+        barRods: [
+          BarChartRodData(
+            toY: 450,
+            rodStackItems: [
+              BarChartRodStackItem(0, 2000000000, widget.dark),
+              BarChartRodStackItem(2000000000, 12000000000, widget.normal),
+              BarChartRodStackItem(12000000000, 17000000000, widget.light),
+            ],
+            borderRadius: BorderRadius.zero,
+            width: barsWidth,
+          ),
+          
+        ],
+      ),
+      BarChartGroupData(
+        x: 4,
+        barsSpace: barsSpace,
+        barRods: [
+          BarChartRodData(
+            toY: 340,
+            rodStackItems: [
+              BarChartRodStackItem(0, 2000000000, widget.dark),
+              BarChartRodStackItem(2000000000, 12000000000, widget.normal),
+              BarChartRodStackItem(12000000000, 17000000000, widget.light),
+            ],
+            borderRadius: BorderRadius.zero,
+            width: barsWidth,
+          ),
+          
+        ],
+      ),
+      BarChartGroupData(
+        x: 5,
+        barsSpace: barsSpace,
+        barRods: [
+          BarChartRodData(
+            toY: 100,
+            rodStackItems: [
+              BarChartRodStackItem(0, 2000000000, widget.dark),
+              BarChartRodStackItem(2000000000, 12000000000, widget.normal),
+              BarChartRodStackItem(12000000000, 17000000000, widget.light),
+            ],
+            borderRadius: BorderRadius.zero,
+            width: barsWidth,
+          ),
+          
+        ],
+      ),
+      BarChartGroupData(
+        x: 6,
+        barsSpace: barsSpace,
+        barRods: [
+          BarChartRodData(
+            toY: 480,
+            rodStackItems: [
+              BarChartRodStackItem(0, 2000000000, widget.dark),
+              BarChartRodStackItem(2000000000, 12000000000, widget.normal),
+              BarChartRodStackItem(12000000000, 17000000000, widget.light),
+            ],
+            borderRadius: BorderRadius.zero,
+            width: barsWidth,
+          ),
+        ],
+      ),
+    ];
+  }
+}
