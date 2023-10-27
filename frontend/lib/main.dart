3b7aa032b92e8e95c47c6e68b0a6663c3e3aa453
Bhaksar <bhaskarmandal369@gmail.com>
2023-10-28 01:14:56 +0530

    firebase initialized

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,19 +1,25 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:spend_sync/bottom_navbar.dart';
 import 'package:spend_sync/const/colors.dart';
+import 'package:spend_sync/firebase_options.dart';
 import 'package:spend_sync/screens/login.dart';
 import 'package:spend_sync/widget/activity/pie_chart.dart';
 
 final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
-void main() {
+void main() async{
   SystemChrome.setSystemUIOverlayStyle(
     //status bar color
     const SystemUiOverlayStyle(
       statusBarColor: Color(0xff131921),
       statusBarIconBrightness: Brightness.light,
     ),
+  );
+   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(const MyApp());
 }
